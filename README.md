3898802ded910e3e8d36547eb45280054ec197ab
Eduardo <ed.luna80@gmail.com>
2022-03-31 21:43:23 -0700

    Add UI to RPS
    Add UI to rock, paper, scissors program with scoreboard, buttons, images
    and title. Update javascript file to execute playRound function on
    click. Rework playRound to count wins and check for winner after each
    round. Add checkWins helper function to run within playRound, update
    scoreboard, and display win or loss message when reaching 5 wins or
    losses.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # RockPaperScissors
 This will be a simple rock paper scissors game played entirely within a browser's console without a GUI. This program will be a way to practice my current skillset with javascript and intends to be revisited later once I know more.
+
+Image credits:
+
+Zoltan Tasi:
+https://foundation.app/@zoltan_tasi
+https://instagram.com/zoltan_tasi
+
+SJ Obijo:
+https://www.instagram.com/sjobjio/
+https://instagram.com/SJ%20OBJIO
+
+Aleksandar Zivkovic:
+https://paypal.me/alekszandar?country.x=RS&locale.x=en_US
+https://instagram.com/instaleksandar
--- script.js
+++ script.js
@@ -1,6 +1,40 @@
 console.log('Hello world');
 
+const playerScore = document.querySelector('.player-score');
 
+const computerScore = document.querySelector('.computer-score');
+
+const winColumn = document.createElement('div');
+
+const scissorsBtn = document.querySelector('#scissors');
+
+const rockBtn = document.querySelector('#rock');
+
+const paperBtn = document.querySelector('#paper');
+
+const content = document.querySelector('.container');
+
+const winMessage = document.createElement('win-message');
+
+const lossMessage = document.createElement('loss-message');
+
+const header = document.querySelector('.header-container');
+
+winMessage.textContent = 'Congratulations, you win!';
+
+lossMessage.textContent = 'Sorry, you lose';
+
+winMessage.style.borderStyle = 'solid';
+
+winMessage.style.borderWidth = '1px';
+
+winMessage.style.color = 'red';
+
+lossMessage.style.borderStyle = 'solid';
+
+lossMessage.style.borderWidth = '1px';
+
+lossMessage.style.color = 'red';
 
 function computerPlay(warray) {
     let random = Math.floor(Math.random() * warray.length);
@@ -11,27 +45,76 @@ const options = ['rock', 'paper', 'scissors'];
 
 
 
-let playerSelection = ' ';
-
-
-
-let computerSelection = computerPlay(options);
-
 /* Declare wins and losses variables to track game outcomes */
 
 let wins = 0;
 
 let losses = 0;
 
-/* Declare function playRound that declares winner of round from playerSelection and computerSelection variables */
+let playerSelection = ' ';
 
-function playRound(playerSelection, computerSelection) {
+const scissors = 'scissors';
+
+const rock = 'rock';
+
+const paper = 'paper';
+
+/* checkWins helper function updates scoreboard after every round */
+
+function checkWins(wins, losses) {
+    if (wins === 5) {
+        document.getElementById('player-wins').innerHTML = '5';
+        content.insertBefore(winMessage, header);
+    }
+
+    else if (losses === 5) {
+        document.getElementById('computer-wins').innerHTML = '5';
+        content.insertBefore(lossMessage, header);
+        
+   }
+    else if (wins === 4) {
+        document.getElementById('player-wins').innerHTML = '4';
+    }
+    else if (losses === 4) {
+        document.getElementById('computer-wins').innerHTML = '4';
+
+    }
+    else if (wins === 3) {
+        document.getElementById('player-wins').innerHTML = '3';
+    }
+    else if (losses === 3) {
+        document.getElementById('computer-wins').innerHTML = '3';
+
+    }   
+    else if (wins === 2) {
+        document.getElementById('player-wins').innerHTML = '2';
+    }
+    else if (losses === 2) {
+        document.getElementById('computer-wins').innerHTML = '2';
+
+    }
+    else if (wins === 1) {
+        document.getElementById('player-wins').innerHTML = '1';
+    }
+    else if (losses === 1) {
+        document.getElementById('computer-wins').innerHTML = '1';
+
+    }
+   
+}
+
+function playRound(playerSelection) {
+    let computerSelection = computerPlay(options);
+    console.log(playerSelection);
+    console.log(computerSelection);
     if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        losses = losses + 1
+        losses = losses + 1;
+        checkWins(wins, losses);
         return 'You lose! Rock beats scissors!'
     }
     else if (playerSelection === 'scissors' && computerSelection === 'paper') {
         wins = wins + 1
+        checkWins(wins, losses);
         return 'You win! Scissors beat paper!'
     }
 
@@ -41,11 +124,13 @@ function playRound(playerSelection, computerSelection) {
 
     else if (playerSelection === 'rock' && computerSelection === 'scissors') {
         wins = wins + 1
+        checkWins(wins, losses);
         return 'You win! Rock beats scissors'
     }
 
     else if (playerSelection === 'rock' && computerSelection === 'paper') {
         losses = losses + 1
+        checkWins(wins, losses);
         return 'You lose! Paper beats rock!'
     }
 
@@ -55,11 +140,13 @@ function playRound(playerSelection, computerSelection) {
 
     else if (playerSelection === 'paper' && computerSelection === 'rock') {
         wins = wins + 1
+        checkWins(wins, losses);
         return 'You win! Paper beats rock'
     }
 
     else if (playerSelection === 'paper' && computerSelection === 'scissors') {
         losses = losses + 1
+        checkWins(wins, losses);
         return 'You lose! Scissors beats paper'
     }
 
@@ -68,29 +155,14 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function whoWins(wins, losses) {
-    if (wins > losses) {
-        console.log('You win the game!')
-    }
-    
-    else if (wins < losses) {
-        console.log('Sorry, you lose.')
-    }
+scissorsBtn.addEventListener('click', () => {
+    playRound(scissors);
+});
 
-    else {
-        console.log('It\'s a draw! Both players have the same score')
-    }
-}
+paperBtn.addEventListener('click', () => {
+    playRound(paper);
+});
 
-
-function game() {
-    for (let i = 0; i < 5; i++) {
-        playerSelection = ((prompt('Pick rock, paper, or scissors', ' ')).toLowerCase());
-        playRound(playerSelection, computerSelection);
-        console.log(playRound(playerSelection, computerSelection));
-    }
-}
-
-game();
-
-whoWins(wins, losses);
+rockBtn.addEventListener('click', () => {
+    playRound(rock);
+});
